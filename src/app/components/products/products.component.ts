0c241f8d0a16cb596610af360802f813fa206ac8
Juan Lopez <jlopezj@proveedor.intercorp.com.pe>
2023-04-26 17:06:24 -0500

    Create home page, refactor app products

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { switchMap } from 'rxjs/operators';
 
 import {
@@ -15,10 +15,10 @@ import { ProductsService } from '../../services/products.service';
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss'],
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent {
   myShoppingCart: Product[] = [];
   total = 0;
-  products: Product[] = [];
+  @Input() products: Product[] = [];
   showProductDetail = false;
   productChosen: Product | null = null;
   limit = 10;
@@ -30,13 +30,6 @@ export class ProductsComponent implements OnInit {
     private productsService: ProductsService
   ) {
     this.myShoppingCart = this.storeService.getShoppingCart();
-  }
-
-  ngOnInit(): void {
-    this.productsService.getAll(10, 0).subscribe((data) => {
-      this.products = data;
-      this.offset += this.limit;
-    });
   }
 
   onAddToShoppingCart(product: Product) {
@@ -103,12 +96,5 @@ export class ProductsComponent implements OnInit {
         this.showProductDetail = false;
       });
     }
-  }
-
-  loadMore() {
-    this.productsService.getAll(this.limit, this.offset).subscribe((data) => {
-      this.products = this.products.concat(data);
-      this.offset += this.limit;
-    });
   }
 }
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import {
+  Product
+} from '../../models/product.model';
+
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  products: Product[] = [];
+  limit = 10;
+  offset = 0;
 
+  constructor(
+    private productsService: ProductsService
+  ) {}
+
+  ngOnInit(): void {
+    this.productsService.getAll(10, 0).subscribe((data) => {
+      this.products = data;
+      this.offset += this.limit;
+    });
+  }
+
+  loadMore() {
+    this.productsService.getAll(this.limit, this.offset).subscribe((data) => {
+      this.products = this.products.concat(data);
+      this.offset += this.limit;
+    });
+  }
 }
