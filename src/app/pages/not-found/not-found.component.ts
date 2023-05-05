302bf16588c7b63c9ce5603c2db65d4604fce545
Juan Lopez <jlopezj@proveedor.intercorp.com.pe>
2023-05-05 13:45:19 -0500

    Page Not Found

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,10 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-not-found',
-  templateUrl: './not-found.component.html',
+  template: `<div><img src="https://media.giphy.com/media/A9EcBzd6t8DZe/giphy.gif" ></div>`,
   styleUrls: ['./not-found.component.scss']
 })
-export class NotFoundComponent {
-
-}
+export class NotFoundComponent { }
